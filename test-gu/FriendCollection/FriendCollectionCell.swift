033bae515b13f96b18e66a22129beae48adb99dd
Sizov Denis <sv_skatch@bk.ru>
2021-10-13 12:31:18 +0300

    Friends UI + Files created

---
+++ test-gu/FriendCollection/FriendCollectionCell.swift
@@ -0,0 +1,14 @@
+//
+//  FriendCollectionCell.swift
+//  test-gu
+//
+//  Created by Денис Сизов on 13.10.2021.
+//
+
+import UIKit
+
+class FriendCollectionCell: UICollectionViewCell {
+    
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendImage: UIImageView!
+}
---
+++ test-gu/Friends/FriendsCell.swift
@@ -0,0 +1,26 @@
+//
+//  FriendCell.swift
+//  test-gu
+//
+//  Created by Денис Сизов on 13.10.2021.
+//
+
+import UIKit
+
+class FriendsCell: UITableViewCell {
+    
+    @IBOutlet weak var friendName: UILabel!
+    @IBOutlet weak var friendAvatar: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
