1915e3bcb3f1de33b7c6eb1389edc3d0ac13383c
Sizov Denis <sv_skatch@bk.ru>
2021-10-13 16:04:55 +0300

    Lesson 3 homework is done

--- test-gu/Search Groups/SearchGroupsController.swift
+++ test-gu/Search Groups/SearchGroupsController.swift
@@ -8,38 +8,40 @@
 import UIKit
 
 class SearchGroupsController: UITableViewController {
+    
+    var groups = GroupsLoader.iNeedGroups()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return groups.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchGroupsCell", for: indexPath) as? SearchGroupsCell else {
+            return UITableViewCell()
+        }
 
-        // Configure the cell...
+        let name = groups[indexPath.row].name
+        let image = groups[indexPath.row].image
+        
+        cell.groupName.text = name
+        cell.groupImage.image = UIImage(named: image)
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
