d12e7403a8d8d22402b5c415a78f471d3a3825a2
Sizov Denis <sv_skatch@bk.ru>
2021-11-01 13:34:43 +0300

    cloud was added

--- test-gu/Login/LoginController.swift
+++ test-gu/Login/LoginController.swift
@@ -30,6 +30,7 @@ class LoginController: UIViewController {
         let hideKeyboardGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         // Присваиваем его UIScrollVIew
         scrollView?.addGestureRecognizer(hideKeyboardGesture)
+        pathAnimation() // отрисуем облачко
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -91,6 +92,63 @@ class LoginController: UIViewController {
         alter.addAction(action)
         // Показываем UIAlertController
         present(alter, animated: true, completion: nil)
+    }
+    
+    // Рисуем облачко
+    func pathAnimation() {
+        let cloudView = UIView()
+
+        view.addSubview(cloudView)
+        cloudView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            cloudView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cloudView.topAnchor.constraint(equalTo: view.topAnchor, constant: 130),
+            cloudView.widthAnchor.constraint(equalToConstant: 120),
+            cloudView.heightAnchor.constraint(equalToConstant: 70)
+        ])
+
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 10, y: 60))
+        path.addQuadCurve(to: CGPoint(x: 20, y: 40), controlPoint: CGPoint(x: 5, y: 50))
+        path.addQuadCurve(to: CGPoint(x: 40, y: 20), controlPoint: CGPoint(x: 20, y: 20))
+        path.addQuadCurve(to: CGPoint(x: 70, y: 20), controlPoint: CGPoint(x: 55, y: 0))
+        path.addQuadCurve(to: CGPoint(x: 90, y: 30), controlPoint: CGPoint(x: 85, y: 15))
+        path.addQuadCurve(to: CGPoint(x: 110, y: 60), controlPoint: CGPoint(x: 110, y: 35))
+        path.close()
+
+        let layerAnimation = CAShapeLayer()
+        layerAnimation.path = path.cgPath
+        layerAnimation.strokeColor = UIColor.white.cgColor
+        layerAnimation.fillColor = UIColor.systemBlue.cgColor
+        layerAnimation.lineWidth = 8
+        layerAnimation.lineCap = .round
+
+        cloudView.layer.addSublayer(layerAnimation)
+
+        let pathAnimationEnd = CABasicAnimation(keyPath: "strokeEnd")
+        pathAnimationEnd.fromValue = 0
+        pathAnimationEnd.toValue = 1
+        pathAnimationEnd.duration = 2
+        pathAnimationEnd.fillMode = .both
+        pathAnimationEnd.isRemovedOnCompletion = false
+        layerAnimation.add(pathAnimationEnd, forKey: nil)
+
+        let pathAnimationStart = CABasicAnimation(keyPath: "strokeStart")
+        pathAnimationStart.fromValue = 0
+        pathAnimationStart.toValue = 1
+        pathAnimationStart.duration = 2
+        pathAnimationStart.fillMode = .both
+        pathAnimationStart.isRemovedOnCompletion = false
+        pathAnimationStart.beginTime = 1
+
+        let animationGroup = CAAnimationGroup()
+        animationGroup.duration = 3
+        animationGroup.fillMode = CAMediaTimingFillMode.backwards
+        animationGroup.animations = [pathAnimationEnd, pathAnimationStart]
+        animationGroup.repeatCount = .infinity
+        layerAnimation.add(animationGroup, forKey: nil)
+        
     }
     
     // Клавиатура появилась
